5b4cd011285322502db0e72dbbf600ed63b28f39
Josh Cheek <josh.cheek@gmail.com>
2011-03-26 09:48:41 -0500

    change .alive?.should(be_true) to .should be_alive

--- spec/spec.rb
+++ spec/spec.rb
@@ -5,7 +5,7 @@ describe GameOfLife do
   context 'when initialized with [0,0], [1,1]' do
     before { @board = GameOfLife.new [0,0] , [1,1] }
     it '[0,0] should be alive' do
-      @board.alive?(0,0).should be_true
+      @board.should be_alive(0,0)
     end
   end
   
