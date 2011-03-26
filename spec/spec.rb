94ce279ca6dffb86815e20db124f7a31ae0c3932
Josh Cheek <josh.cheek@gmail.com>
2011-03-26 11:13:20 -0500

    use 'it' instead of 'specify' for some of the specs, switch all spelling of neighbor to neighbour for consistency

--- spec/spec.rb
+++ spec/spec.rb
@@ -5,27 +5,27 @@ describe GameOfLife do
   
   context 'when initialized with [0,0], [1,1]' do
     subject { GameOfLife.new [0,0] , [1,1] }
-    specify { should      be_alive_at(0,0) }
-    specify { should_not  be_alive_at(0,1) }
-    specify { should_not  be_alive_at(1,0) }
-    specify { should      be_alive_at(1,1) }
+    it      { should      be_alive_at(0,0) }
+    it      { should_not  be_alive_at(0,1) }
+    it      { should_not  be_alive_at(1,0) }
+    it      { should      be_alive_at(1,1) }
     
-    context 'neigbors' do
-      check_neighbors = lambda do |x,y,neighbors|
+    context 'neigbours' do
+      check_neighbours = lambda do |x,y,neighbours|
         specify do
-          subject.neighbours(x,y).should equal(neighbors) ,
-            "[#{x},#{y}] should have #{neighbors} neighbours"
+          subject.neighbours(x,y).should equal(neighbours) ,
+            "[#{x},#{y}] should have #{neighbours} neighbours"
         end
       end
-      check_neighbors.call  -2,-2 , 0
-      check_neighbors.call  -1,-1 , 1
-      check_neighbors.call   0,0  , 1
-      check_neighbors.call   1,1  , 1
-      check_neighbors.call   0,1  , 2
-      check_neighbors.call   1,0  , 2
-      check_neighbors.call  10,0  , 0
-      check_neighbors.call   0,10 , 0
+      check_neighbours.call  -2,-2 , 0
+      check_neighbours.call  -1,-1 , 1
+      check_neighbours.call   0,0  , 1
+      check_neighbours.call   1,1  , 1
+      check_neighbours.call   0,1  , 2
+      check_neighbours.call   1,0  , 2
+      check_neighbours.call  10,0  , 0
+      check_neighbours.call   0,10 , 0
     end
   end
-
+  
 end
