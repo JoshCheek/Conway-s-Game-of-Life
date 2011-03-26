0126495a678bcefd75a4817c431e8866a8a47c09
Josh Cheek <josh.cheek@gmail.com>
2011-03-26 09:52:10 -0500

    refactor specs to use implicit subject

--- spec/spec.rb
+++ spec/spec.rb
@@ -3,19 +3,14 @@ require 'life'
 describe GameOfLife do
   
   context 'when initialized with [0,0], [1,1]' do
-    before { @board = GameOfLife.new [0,0] , [1,1] }
-    specify '[0,0] should be alive' do
-      @board.should be_alive(0,0)
-    end
-    specify '[0,1] should not be alive' do
-      @board.should_not be_alive(0,1)
-    end
-    specify '[1,0] should not be alive' do
-      @board.should_not be_alive(1,0)
-    end
-    specify '[1,1] should not be alive' do
-      @board.should be_alive(1,1)
-    end
+    subject { GameOfLife.new [0,0] , [1,1] }
+    specify { should      be_alive(0,0) }
+    specify { should_not  be_alive(0,1) }
+    specify { should_not  be_alive(1,0) }
+    specify { should      be_alive(1,1) }
+    # specify '[0,0] should have one neighbour' do
+    #   @board 
+    # end
   end
   
 end
