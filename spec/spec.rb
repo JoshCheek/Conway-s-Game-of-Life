54915fc0c3160976c51fc887fa89aa9461b51970
Josh Cheek <josh.cheek@gmail.com>
2011-03-26 10:25:39 -0500

    use custom rspec matcher

--- spec/spec.rb
+++ spec/spec.rb
@@ -1,16 +1,14 @@
+require 'spec_helper'
 require 'life'
 
 describe GameOfLife do
   
   context 'when initialized with [0,0], [1,1]' do
     subject { GameOfLife.new [0,0] , [1,1] }
-    specify { should      be_alive(0,0) }
-    specify { should_not  be_alive(0,1) }
-    specify { should_not  be_alive(1,0) }
-    specify { should      be_alive(1,1) }
-    # specify '[0,0] should have one neighbour' do
-    #   @board 
-    # end
+    specify { should      be_alive_at(0,0) }
+    specify { should_not  be_alive_at(0,1) }
+    specify { should_not  be_alive_at(1,0) }
+    specify { should      be_alive_at(1,1) }
   end
-  
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,21 @@
 $LOADED_FEATURES << File.dirname(__FILE__) + '/../lib'
+
+RSpec::Matchers.define :be_alive_at do |x,y|
+  
+  match do |board|
+    board.alive? x , y
+  end  
+  
+  failure_message_for_should do
+    "expected [#{x},#{y}] to be dead"
+  end
+  
+  failure_message_for_should_not do
+    "expected [#{x},#{y}] to be alive"
+  end
+  
+  description do
+    "be alive at [#{x},#{y}]"
+  end
+  
+end
